4bed70794dc8c14dacab55b6c1c157ed0df3c79c
名久井涼佑 <nakuiryosuke@dmpfile.local>
2022-10-05 00:53:14 +0900

    feat: ヘッダーコンポーネント実装

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <div class="header__inner">
+      <h1 class="header-title">RESAS総人口推移グラフ</h1>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+  .header {
+    background-color: $color_blue;
+    &__inner {
+      width: 100%;
+      max-width: 1080px;
+      padding: 18px 0 18px 15px;
+    }
+  }
+
+  .header-title {
+    font-size: 24px;
+    font-weight: bold;
+    color: $color_white;
+  }
+</style>
