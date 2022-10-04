5405d1639608e5eb04b712bac2d2e762cd54795c
名久井涼佑 <nakuiryosuke@dmpfile.local>
2022-10-05 00:55:07 +0900

    feat: 都道府県一覧コンポーネント実装

---
+++ src/components/PrefList.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import type { PrefObject } from 'Resas'
+
+  interface Props {
+    data: PrefObject | undefined
+  }
+
+  interface Emits {
+    (e: 'updatePref', value: number[]): void
+  }
+
+  const props = defineProps<Props>()
+  const emit = defineEmits<Emits>()
+
+  const checkedPrefCode = ref<number[]>([])
+  const handlerCheckbox = (prefCode: number) => {
+    if (checkedPrefCode.value.includes(prefCode)) {
+      const i = checkedPrefCode.value.indexOf(prefCode)
+      checkedPrefCode.value.splice(i, 1)
+    } else {
+      checkedPrefCode.value.push(prefCode)
+    }
+
+    emit('updatePref', checkedPrefCode.value)
+  }
+</script>
+
+<template>
+  <div class="pref">
+    <div class="pref__inner">
+      <h2 class="pref-title">都道府県</h2>
+      <ul v-if="props.data" class="pref-list">
+        <li
+          v-for="(pref, i) in props.data.result"
+          :key="i"
+          class="pref-list__item"
+        >
+          <label :for="`pref_${pref.prefCode}`">
+            <input
+              type="checkbox"
+              :name="`${pref.prefCode}`"
+              :id="`pref_${pref.prefCode}`"
+              @change="handlerCheckbox(pref.prefCode)"
+            />{{ pref.prefName }}
+          </label>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .pref {
+    margin: 25px auto 50px;
+    &__inner {
+      width: 100%;
+      max-width: 1080px;
+      margin: 0 auto;
+    }
+  }
+
+  .pref-title {
+    font-size: 20px;
+    font-weight: bold;
+    color: $color_pink;
+  }
+
+  .pref-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+    justify-content: center;
+    align-items: center;
+    gap: 50px 30px;
+    padding: 40px 50px 30px;
+    list-style: none;
+    &__item {
+      font-size: 20px;
+      font-weight: bold;
+      > label {
+        cursor: pointer;
+        > input {
+          margin-right: 8px;
+        }
+      }
+    }
+  }
+</style>
