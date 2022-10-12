eaf8cad3443801f3c047b9cdc6a7ae21f5200363
名久井涼佑 <nakuiryosuke@dmpfile.local>
2022-10-13 01:56:57 +0900

    feat: Resasの型定義追加（人口構成）

--- src/@types/resas.d.ts
+++ src/@types/resas.d.ts
@@ -1,11 +1,37 @@
 declare module 'Resas' {
   export interface PrefObject {
     message?: any
-    result: Result[]
+    result: PrefResult[]
   }
 
-  export interface Result {
+  interface PrefResult {
     prefCode: number
     prefName: string
+  }
+
+  export interface GraphObject {
+    message?: any
+    result: GraphResult
+  }
+
+  interface GraphResult {
+    boundaryYear: number
+    data: Datum2[]
+  }
+
+  interface Datum2 {
+    label: string
+    data: Datum[]
+  }
+
+  interface Datum {
+    year: number
+    value: number
+    rate?: number
+  }
+
+  export interface GraphData {
+    code: number
+    data: GraphObject
   }
 }
